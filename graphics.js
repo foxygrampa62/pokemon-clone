d6692a3cebee7bec9596ccc201ee4452b8d3a345
Luke Foye <mcrnm62@gmail.com>
2022-01-02 23:21:14 -0700

    bulbasaur
    bulbasaur

--- graphics.js
+++ graphics.js
@@ -1,18 +1,15 @@
 
 let initializeGraphics = function (){
     let canvas = document.createElement("canvas")
-    let context = canvas.getContext("2d") 
-    let myImage = new Image(100, 100);
-    let imageElement = document.createElement ("img")
-    imageElement.setAttribute ("src", "/sprites/6.png" )
-    imageElement.setAttribute ("width", "100" )
-    imageElement.setAttribute ("height", "100" )
-    myImage.src = "/sprites/6.png"
-    myImage.onload = function (){
-        context.drawImage (imageElement, 50, 50, 50, 50)
-    }
-    
-    console.log(context)
     document.body.appendChild(canvas)
-    document.body.appendChild(imageElement)
+}
+let renderEnemySprite = function(pokemonID){
+    let myImage = new Image(100, 100);
+    
+    myImage.src = "/sprites/" + pokemonID + ".png"
+    myImage.onload = function (){
+        let canvas = document.querySelector ("canvas")
+        let context = canvas.getContext("2d") 
+        context.drawImage (myImage, 50, 50, 50, 50)
+    }
 }
