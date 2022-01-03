10f1d4661ef2a858011c0b1085ef672a4076833e
Luke Foye <mcrnm62@gmail.com>
2022-01-02 23:05:27 -0700

    Update graphics.js

--- graphics.js
+++ graphics.js
@@ -2,7 +2,17 @@
 let initializeGraphics = function (){
     let canvas = document.createElement("canvas")
     let context = canvas.getContext("2d") 
+    let myImage = new Image(100, 100);
+    let imageElement = document.createElement ("img")
+    imageElement.setAttribute ("src", "/sprites/6.png" )
+    imageElement.setAttribute ("width", "100" )
+    imageElement.setAttribute ("height", "100" )
+    myImage.src = "/sprites/6.png"
+    myImage.onload = function (){
+        context.drawImage (imageElement, 50, 50, 50, 50)
+    }
+    
     console.log(context)
     document.body.appendChild(canvas)
-
+    document.body.appendChild(imageElement)
 }
