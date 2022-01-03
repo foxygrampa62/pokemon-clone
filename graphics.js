ac44d12023a1024eeb48c78cefd4f22363dee8bd
Luke Foye <mcrnm62@gmail.com>
2022-01-02 23:49:50 -0700

    geodude

--- graphics.js
+++ graphics.js
@@ -17,6 +17,16 @@ let renderEnemySprite = function(pokemonID){
     myImage.onload = function (){
         let canvas = document.querySelector ("canvas")
         let context = canvas.getContext("2d") 
-        context.drawImage (myImage, 250, 50, 150, 150)
+        context.drawImage (myImage, 200, 25, 200, 200)
+    }
+}
+let renderPlayerSprite = function(pokemonID){
+    let myImage = new Image(100, 100);
+    
+    myImage.src = "/sprites/back/" + pokemonID + ".png"
+    myImage.onload = function (){
+        let canvas = document.querySelector ("canvas")
+        let context = canvas.getContext("2d") 
+        context.drawImage (myImage, 0, 125, 200, 200)
     }
 }
