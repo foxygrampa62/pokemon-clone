79d9673fea7bc63ecc6f531b4446b54f3634cca2
Luke Foye <mcrnm62@gmail.com>
2022-01-02 22:39:35 -0700

    Update graphics.js

--- graphics.js
+++ graphics.js
@@ -1,5 +1,8 @@
 
 let initializeGraphics = function (){
     let canvas = document.createElement("canvas")
+    let context = canvas.getContext("2d") 
+    console.log(context)
     document.body.appendChild(canvas)
+
 }
